92a64ba81b594bb885108899586d190dcf211644
Terence Ma <awstest135@gmail.com>
2020-11-29 12:10:04 +1100

    Added a calendar package

---
+++ js/eventSourceExample.js
@@ -0,0 +1,30 @@
+$('#calendar').fullCalendar({
+
+  eventSources: [
+
+    // your event source
+    {
+      events: [ // put the array in the `events` property
+        {
+          title  : 'event1',
+          start  : '2010-01-01'
+        },
+        {
+          title  : 'event2',
+          start  : '2010-01-05',
+          end    : '2010-01-07'
+        },
+        {
+          title  : 'event3',
+          start  : '2010-01-09T12:30:00',
+        }
+      ],
+      color: 'black',     // an option!
+      textColor: 'yellow' // an option!
+    }
+
+    // any other event sources...
+
+  ]
+
+});
---
+++ js/index.js
@@ -0,0 +1,10 @@
+if (typeof(Storage) !== "undefined") {
+  // Store
+  localStorage.setItem("lastname", "Smith");
+  // Retrieve
+  document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+} else {
+  document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+
+
