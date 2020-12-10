7052f998a417b6bc3e483cc25683900fdd4a1113
hippee <huuhiep@hotmail.com>
2020-12-10 14:00:26 +1100

    task4

--- save.js
+++ save.js
@@ -1,6 +1,12 @@
 
 
-var elements= [];
+ let elements= [];
+ 
+let taskDescription = document.queryBySelector('#taskDetails');
+let taskAssigned = document.querySelector('#inputAssign');
+let taskDueDate = document.querySelector('#inputDate');
+let taskStatus = document.querySelector('#taskStatus');
+ 
 //storage the data in window
 window.onload = function(){
     if(JSON.parse(localStorage.getItem("taskForm"))!==null){
@@ -13,11 +19,17 @@ window.onload = function(){
 //end storage window
 
 function addElement(){
-    if (document.querySelector(".addForm1,.addForm2,.addForm3").value.trim() !=""){
-        elements.push(document.querySelector(".addForm1,.addForm2,.addForm3").value.trim());
+    if ((taskDescription && taskAssigned) == ""){
+        console.error('invalid Input');
+    } else {
+        elements.push(taskDescription.value);
+        elements.push(taskAssigned.value);
+        elements.push(taskDueDate.value);
+        elements.push(taskStatus.value);
         console.log(elements);
         alert(elements);      
     }
+}
  //add local STORAGE
  if (localStorage.getItem("taskForm")==null){
     localStorage.setItem("taskForm",JSON.stringify(elements));
@@ -29,12 +41,55 @@ else{
 console.log(localStorage.getItem("taskForm"));
 //end local storage
     display();
-}
+
 function display(){
-    document.querySelector(".addForm1,.addForm2,.addForm3").innerHTML = "Enter here";
+    taskDescription.innerHTML = "Enter here";
+    taskAssigned.innerHTML = "Enter here";
     for (let i=0;i<elements.length;i++){
-        document.querySelector(".addForm1,.addForm2,.addForm3").innerHTML += elements[i];
+        taskDescription.innerHTML += elements[i];
+        taskAssigned.innerHTML += elements[i];
         console.log(elements[i]);
     }
 }
 
+
+
+/*
+// Select the New Task Form
+const newTaskForm = document.querySelector('#newTaskForm');
+
+// Add an 'onsubmit' event listener
+newTaskForm.addEventListener('submit', (event) => {
+    // Prevent default action
+    event.preventDefault();
+
+    // Select the inputs
+    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+    const newTaskDescription = document.querySelector('#newTaskDescription');
+    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+    const newTaskDueDate = document.querySelector('#newTaskDueDate');
+    const errorMessage = document.querySelector('#alertMessage');
+    
+    
+    /*
+        Validation code here
+    */
+
+    // Get the values of the inputs
+    /*const name = newTaskNameInput.value;
+    const description = newTaskDescription.value;
+    const assignedTo = newTaskAssignedTo.value;
+    const dueDate = newTaskDueDate.value;
+    if(!validFormFieldInput(name)){
+        errorMessage.innerHTML = "Invalid name input";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
+    }
+
+});
+
+function validFormFieldInput(data){
+    return data !== null && data !== '';
+}
+*/
